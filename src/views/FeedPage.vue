20527a362326d12c8ecb4f81b36762728375d7bc
FaikYY <yazilimcifaik@gmail.com>
2023-05-07 22:36:50 -0500

    First commit, routers created

---
+++ src/views/FeedPage.vue
@@ -0,0 +1,10 @@
+<template>
+<h1>Feed Page</h1>
+</template>
+
+<script>
+
+</script>
+
+<style>
+</style>
---
+++ src/views/SignInPage.vue
@@ -0,0 +1,10 @@
+<template>
+<h1>Sign In Page</h1>
+</template>
+
+<script>
+
+</script>
+
+<style>
+</style>
